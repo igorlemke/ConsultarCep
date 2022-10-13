677296fc24e26d12dca03db84904834055969dbf
Igor Lemke <igorlemke95@gmail.com>
2022-10-12 22:09:43 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from interface_grafica import *
+from consulta_cep import *
+
+tela_inicial()
+while True:
+    window, event, values = sg.read_all_windows()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == 'Consultar':
+        try:
+            logradouro = consulta(values['cep'])['logradouro']
+            bairro = consulta(values['cep'])['bairro']
+            localidade = consulta(values['cep'])['localidade']
+            uf = consulta(values['cep'])['uf']
+            ibge = consulta(values['cep'])['ibge']
+            ddd = consulta(values['cep'])['ddd']
+
+            window['logradouro'].update(logradouro)
+            window['bairro'].update(bairro)
+            window['localidade'].update(localidade)
+            window['uf'].update(uf)
+            window['ibge'].update(ibge)
+            window['ddd'].update(ddd)
+
+        except:
+            sg.Popup('Verifique se o campo "CEP" foi preenchido corretamente\n'
+                                               'ou sua conexão com a internet!', font='arial 12', title='ERRO')
---
+++ interface_grafica.py
@@ -0,0 +1,42 @@
+import PySimpleGUI as sg
+
+def tela_inicial():
+    sg.theme('Dark2')
+
+    cep = [
+        [sg.Text('Informe um CEP:', font='arial 12', pad=(0, 0))],
+        [sg.Input(size=(20, 0), font='arial 12', pad=(0, 0), key='cep')]
+    ]
+
+    coluna1 = [
+        [sg.Text('LOGRADOURO:', font='arial 12')],
+        [sg.Text('BAIRRO:', font='arial 12')],
+        [sg.Text('LOCALIDADE:', font='arial 12')],
+        [sg.Text('UF:', font='arial 12')],
+        [sg.Text('DDD:', font='arial 12')],
+        [sg.Text('IBGE:', font='arial 12')]
+    ]
+
+    coluna2 = [
+        [sg.Input(font='arial 12 bold', key='logradouro', size=(35, 1))],
+        [sg.Input(font='arial 12 bold', key='bairro', size=(35, 1))],
+        [sg.Input(font='arial 12 bold', key='localidade', size=(35, 1))],
+        [sg.Input(font='arial 12 bold', key='uf', size=(4, 1))],
+        [sg.Input(font='arial 12 bold', key='ddd', size=(4, 1))],
+        [sg.Input(font='arial 12 bold', key='ibge', size=(15, 1))]
+    ]
+
+    botoes = [
+        [sg.Button('Consultar', font='arial 12', size=(10, 1), pad=((0, 15), 0)),
+        sg.CButton('Sair', font='arial 12', size=(8, 1))]
+    ]
+
+    layout = [
+        [sg.Text('Consultar CEP e código do IBGE', font='arial 18 bold')],
+        [sg.Column(cep, justification='center', element_justification='center')],
+        [sg.Column(coluna1, pad=((0, 20), 0)),
+         sg.Column(coluna2)],
+        [sg.Column(botoes, justification='center')]
+    ]
+
+    telaprin = sg.Window('Consultar CEP', element_padding=(0, 10), layout=layout, size=(600, 500), finalize=True)
